db9afaa999dd4a253cec8a2e06d3231d9d1896ca
Vitalii Maksymenko <maxymenkovitaly@gmail.com>
2023-11-03 17:22:06 +0100

    implement changes of the style

--- script.js
+++ script.js
@@ -2,38 +2,6 @@
 
 // //💥📈📉⛔️🎉
 
-// let score = 20;
-
-// document.querySelector('.number').textContent = secretNumber;
-
-// document.querySelector('.check').addEventListener('click', function () {
-//   const guess = Number(document.querySelector('.guess').value);
-
-//   if (!guess) {
-//     document.querySelector('.message').textContent = '⛔️ No number';
-//   } else if (guess === secretNumber) {
-//     document.querySelector('.message').textContent = '🎉 Correct number';
-//   } else if (guess > secretNumber) {
-//     if (score > 0) {
-//       document.querySelector('.message').textContent = '📈 Too high!';
-//       score--;
-//       document.querySelector('.score').textContent = score;
-//     } else {
-//       document.querySelector('.message').textContent = '💥 You lost the game!';
-//       document.querySelector('.score').textContent = 0;
-//     }
-//   } else if (guess < secretNumber) {
-//     if (score > 0) {
-//       document.querySelector('.message').textContent = '📉 Too low!';
-//       score--;
-//       document.querySelector('.score').textContent = score;
-//     } else {
-//       document.querySelector('.message').textContent = '💥 You lost the game!';
-//       document.querySelector('.score').textContent = 0;
-//     }
-//   }
-// });
-
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
@@ -43,10 +11,18 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No number';
+
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct number';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 0) {
       document.querySelector('.message').textContent = '📈 Too high!';
@@ -56,6 +32,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '💥 You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+
+    // When guess is too low
   } else if (guess < secretNumber) {
     if (score > 0) {
       document.querySelector('.message').textContent = '📉 Too low!';
